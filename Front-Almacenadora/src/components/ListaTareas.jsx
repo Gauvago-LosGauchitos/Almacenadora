4b4f1fadaa9fbebb3d648d5cda79d5344005e4dd
axitumul-2022038 <axitumul-2022038@kinal.edu.gt>
2024-05-04 10:22:25 -0600

    arreglo de la tabla al borrar y actualizar

--- Front-Almacenadora/src/components/ListaTareas.jsx
+++ Front-Almacenadora/src/components/ListaTareas.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react"
-import { Input } from "./Input"
-import { useTask } from "../shared/hooks/useTask.jsx"
+import { useState, useEffect } from "react";
+import { Input } from "./Input";
+import { useTask } from "../shared/hooks/useTask.jsx";
 import {
     descriptionValidationMessage,
     fechaFinValidationMessage,
@@ -11,15 +11,12 @@ import {
     validateFechaInicio,
     validateNombre,
     validateNombreYapellidoPersona
-} from "../shared/validators/validator.js"
-import toast from 'react-hot-toast'
-import './ListaTareas.css'
+} from "../shared/validators/validator.js";
+import toast from 'react-hot-toast';
+import './ListaTareas.css';
 
 export const TodoListForm = () => {
-    const { addTask, updateTask, deleteTask, isLoading, getTasks, tasks } = useTask()
-
-    const [tasksC, setTaskC] = useState([])
-    const [isLoadingTasks, setIsLoadingTasks] = useState(true)
+    const { addTask, updateTask, deleteTask, isLoading, getTasks, tasks } = useTask();
 
     const [formData, setFormData] = useState({
         nombre: {
@@ -47,66 +44,59 @@ export const TodoListForm = () => {
             isValid: false,
             showError: false
         }
-    })
+    });
 
     const isSubmitButtonDisabled =
         !formData.nombre.isValid ||
         !formData.description.isValid ||
         !formData.fechaInicio.isValid ||
         !formData.fechaFin.isValid ||
-        !formData.nombreYapellidoPersona.isValid
+        !formData.nombreYapellidoPersona.isValid;
 
-
-        
-        const fetchTasks = async () => {
-            try {
-              setTaskC(tasks)
-            } catch (error) {
-              console.error("Error al obtener las tareas en fetchTasks:", error)
-            }finally{
-                setIsLoadingTasks(false)
-            }
-          }
+    const fetchTasks = async () => {
+        try {
+            await getTasks();
+        } catch (error) {
+            console.error("Error al obtener las tareas en fetchTasks:", error);
+        }
+    };
 
     useEffect(() => {
-          fetchTasks() 
-          setTaskC(tasks)         
-    }, [])
-
-
+        fetchTasks();
+    }, []);
 
     const handleValueChange = (value, field) => {
-        setFormData((prevData) => ({
+        setFormData(prevData => ({
             ...prevData,
             [field]: {
                 ...prevData[field],
                 value
             }
-        }))
-    }
+        }));
+    };
 
     const handleValidationOnBlur = (value, field) => {
-        let isValid = false
+        let isValid = false;
         switch (field) {
             case "nombre":
-                isValid = validateNombre(value)
-                break
+                isValid = validateNombre(value);
+                break;
             case "description":
-                isValid = validateDescription(value)
-                break
+                isValid = validateDescription(value);
+                break;
             case "fechaInicio":
-                isValid = validateFechaInicio(value)
-                break
+                isValid = validateFechaInicio(value);
+                break;
             case "fechaFin":
-                isValid = validateFechaFin(value)
-                break
+                isValid = validateFechaFin(value);
+                break;
             case "nombreYapellidoPersona":
-                isValid = validateNombreYapellidoPersona(value)
-                break
+                isValid = validateNombreYapellidoPersona(value);
+                break;
             default:
-                break
+                break;
         }
-        setFormData((prevData) => ({
+        setFormData(prevData => ({
             ...prevData,
             [field]: {
                 ...prevData[field],
@@ -114,46 +104,53 @@ export const TodoListForm = () => {
                 isValid,
                 showError: !isValid
             }
-        }))
-    }
+        }));
+    };
 
     const handleAddTask = async (e) => {
-        e.preventDefault()
-        addTask(
-            formData.nombre.value,
-            formData.description.value,
-            formData.fechaInicio.value,
-            formData.fechaFin.value,
-            formData.nombreYapellidoPersona.value
-        )
-        setFormData({
-            nombre: {
-              value: "",
-              isValid: false,
-              showError: false
-            },
-            description: {
-              value: "",
-              isValid: false,
-              showError: false
-            },
-            fechaInicio: {
-              value: "",
-              isValid: false,
-              showError: false
-            },
-            fechaFin: {
-              value: "",
-              isValid: false,
-              showError: false
-            },
-            nombreYapellidoPersona: {
-              value: "",
-              isValid: false,
-              showError: false
-            }
-          })
-    }
+        e.preventDefault();
+        try {
+            await addTask(
+                formData.nombre.value,
+                formData.description.value,
+                formData.fechaInicio.value,
+                formData.fechaFin.value,
+                formData.nombreYapellidoPersona.value
+            );
+            toast.success('Tarea agregada correctamente');
+            setFormData({
+                nombre: {
+                    value: "",
+                    isValid: false,
+                    showError: false
+                },
+                description: {
+                    value: "",
+                    isValid: false,
+                    showError: false
+                },
+                fechaInicio: {
+                    value: "",
+                    isValid: false,
+                    showError: false
+                },
+                fechaFin: {
+                    value: "",
+                    isValid: false,
+                    showError: false
+                },
+                nombreYapellidoPersona: {
+                    value: "",
+                    isValid: false,
+                    showError: false
+                }
+            });
+            fetchTasks();
+        } catch (error) {
+            console.error('Error al agregar la tarea:', error);
+            toast.error('Error al agregar la tarea');
+        }
+    };
 
     const handleUpdateTask = async (taskId) => {
         try {
@@ -163,25 +160,27 @@ export const TodoListForm = () => {
                 fechaInicio: formData.fechaInicio.value,
                 fechaFin: formData.fechaFin.value,
                 nombreYapellidoPersona: formData.nombreYapellidoPersona.value
-            })
-            toast.success('Tarea actualizada correctamente')
+            });
+            toast.success('Tarea actualizada correctamente');
+            fetchTasks();
         } catch (error) {
-            console.error('Error al actualizar la tarea:', error)
-            toast.error('Error al actualizar la tarea')
+            console.error('Error al actualizar la tarea:', error);
+            toast.error('Error al actualizar la tarea');
         }
-    }
+    };
 
     const handleDeleteTask = async (taskId) => {
         if (window.confirm('¿Estás seguro de que quieres eliminar esta tarea?')) {
             try {
-                await deleteTask(taskId)
-                toast.success('Tarea eliminada correctamente')
+                await deleteTask(taskId);
+                toast.success('Tarea eliminada correctamente');
+                fetchTasks();
             } catch (error) {
-                console.error('Error al eliminar la tarea:', error)
-                toast.error('Error al eliminar la tarea')
+                console.error('Error al eliminar la tarea:', error);
+                toast.error('Error al eliminar la tarea');
             }
         }
-    }
+    };
 
     return (
         <div className="todo-list-container">
@@ -259,33 +258,31 @@ export const TodoListForm = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            tasksC && tasksC.map((task, index) => (
-
-                                <tr key={index}>
-                                    <td>{task.nombre}</td>
-                                    <td>{task.description}</td>
-                                    <td>{task.fechaInicio}</td>
-                                    <td>{task.fechaFin}</td>
-                                    <td>{task.nombreYapellidoPersona}</td>
-                                    <td>
-                                        <button className="edit-button" onClick={() => handleUpdateTask(task._id)}>
-                                            <svg class="edit-svgIcon" viewBox="0 0 512 512">
-                                                <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
-                                            </svg>
-                                        </button>
-                                        <button className="delete-button" onClick={() => handleDeleteTask(task._id)}>
-                                            <svg class="delete-svgIcon" viewBox="0 0 448 512">
-                                                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
-                                            </svg>
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))
-                        }
+                        {tasks && tasks.length > 0 && tasks.map((task, index) => (
+                            <tr key={index}>
+                                <td>{task.nombre}</td>
+                                <td>{task.description}</td>
+                                <td>{task.fechaInicio}</td>
+                                <td>{task.fechaFin}</td>
+                                <td>{task.nombreYapellidoPersona}</td>
+                                <td>
+                                    <button className="edit-button" onClick={() => handleUpdateTask(task._id)}>
+                                        <svg className="edit-svgIcon" viewBox="0 0 512 512">
+                                        <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
+                                        </svg>
+                                    </button>
+                                    <button className="delete-button" onClick={() => handleDeleteTask(task._id)}>
+                                        <svg className="delete-svgIcon" viewBox="0 0 448 512">
+                                        <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
+
                 </table>
             </div>
         </div>
-    )
-}
+    );
+};
